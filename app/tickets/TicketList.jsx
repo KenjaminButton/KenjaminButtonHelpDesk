1a47a39415b6e6a6dd9692326029b78d41dc17b7
KenjaminButton <kennethpchang@gmail.com>
2023-09-05 06:07:23 +0800

    Fixed Missing 'key' prop for element in iterator react/jsx-key, error for npm run build

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -21,15 +21,16 @@ export default async function TicketList() {
   return (
     <>
       {tickets.map( (ticket) => (
-        <Link href={`/tickets/${ticket.id}`}>
-          <div key={ticket.id} className="card my-5">
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
               {ticket.priority} priority
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
+        
       ))}
       {tickets.length === 0 && (
         <p className="text-center">No open tickets! Yipee!</p>
