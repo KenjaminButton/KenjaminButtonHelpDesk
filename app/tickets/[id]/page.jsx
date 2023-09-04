84177a92b943d2479cefbd9225db7d0567e37e94
KenjaminButton <kennethpchang@gmail.com>
2023-09-05 04:27:54 +0800

    Static rendering using generateStaticParams to have all tickets pre-rendered during build

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,31 @@
+
+import {notFound} from 'next/navigation'
+
+// if a request comes in for a ticket id that hasn't been rendered, we can return a 404 page
+export const dynamicParams = true
+
+// Get a list of all id (tickets) at build time and generate a static page (and generate all the routes) for all of them pre-rendered during build
+export async function generateStaticParams() {
+  // [{id: '1'}, {id: '2'}, ...]
+  const res = await fetch('http://localhost:4000/tickets')
+  const tickets = await res.json()
+
+  return tickets.map( (ticket) => ({
+    id: ticket.id
+  }))
+}
+
 async function getTicket(id) {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
       revalidate: 60
     }
   })
+
+  if (!res.ok) {
+    notFound()
+  }
+
   return res.json()
 }
 
