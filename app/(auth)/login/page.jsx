b463c69b3f32e1a5bb4983404a59a3f7b0117f6b
KenjaminButton <kennethpchang@gmail.com>
2023-09-05 06:45:41 +0800

    Restructured folders for route groups

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,10 @@
+export default function Login() {  
+  return (
+    <main>
+      <h2 className="text-center">Login</h2>
+    </main>
+  )
+}
+
+// /auth/login vs /login (without the auth before)
+// Accomplish this by having auth folder renamed to (auth)
