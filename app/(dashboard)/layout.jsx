5eeda4e849e7338194b9d7f8d8d1bc4b79880166
KenjaminButton <kennethpchang@gmail.com>
2023-09-07 03:25:38 +0800

    Protected tickets from unauthenticated users

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -2,12 +2,16 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 // Components
 import Navbar from "../components/Navbar";
+import { redirect } from "next/navigation";
 
 
 export default async function DashboardLayout({children}) {
   const supabase = createServerComponentClient({cookies})
   const {data} = await supabase.auth.getSession()
 
+  if (!data.session) {
+    redirect('/login')
+  }
 
   return (
     <>
