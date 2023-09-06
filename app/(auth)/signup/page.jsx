b6d258b72a3b2532e9729c8270779f4518834eb8
KenjaminButton <kennethpchang@gmail.com>
2023-09-07 02:24:18 +0800

    Signup for New User working, but without verify email callback

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,17 +1,44 @@
 "use client"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import AuthForm from "../AuthForm"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
+
 
 export default function Signup() {  
+  const router = useRouter()
+  const [formError, setFormError] = useState('')
 
   const handleSubmit = async (evt, email, password) => {
     evt.preventDefault()
-    console.log("User signup", email, password)
+    // console.log("User signup", email, password)
+
+    const supabase = createClientComponentClient()
+    const {error} = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    })
+
+    if (error) {
+      // error from supabase
+      setFormError(error.message)
+    }
+    if (!error) {
+      router.push(`/verify`)
+    }
   }
 
   return (
     <main>
       <h2 className="text-center">Signup</h2>
       <AuthForm handleSubmit={handleSubmit} />
+      {formError && (
+        <div className="error">{formError}</div>
+      )}
     </main>
   )
 }
